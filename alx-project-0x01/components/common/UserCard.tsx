3ad8efecce9dbf4e5e2f1139fcc22d49f76edddd
Banafo <surmjnr@gmail.com>
2025-06-13 12:43:18 +0000

    implementing the user card

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { UserProps } from '../../interfaces';
+import styles from './UserCard.module.css';
+
+interface UserCardProps {
+  user: UserProps;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <h2 className={styles.name}>{user.name}</h2>
+        <span className={styles.username}>@{user.username}</span>
+      </div>
+      
+      <div className={styles.content}>
+        <div className={styles.info}>
+          <p className={styles.email}>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p className={styles.phone}>
+            <strong>Phone:</strong> {user.phone}
+          </p>
+          <p className={styles.website}>
+            <strong>Website:</strong> {user.website}
+          </p>
+        </div>
+
+        <div className={styles.address}>
+          <h3>Address</h3>
+          <p>{user.address.street}, {user.address.suite}</p>
+          <p>{user.address.city}, {user.address.zipcode}</p>
+          <p>Location: {user.address.geo.lat}, {user.address.geo.lng}</p>
+        </div>
+
+        <div className={styles.company}>
+          <h3>Company</h3>
+          <p className={styles.companyName}>{user.company.name}</p>
+          <p className={styles.catchPhrase}>{user.company.catchPhrase}</p>
+          <p className={styles.bs}>{user.company.bs}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard; 
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -32,14 +32,14 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
 };
 
 export async function getStaticProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await res.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
 
   return {
     props: {
-      users,
-    },
-  };
+      users
+    }
+  }
 }
 
 export default UsersPage; 
